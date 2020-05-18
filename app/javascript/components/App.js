e7c15153c6a6611881bd0f3f806c75b9ce582a44
Eusebius Marcel <eusebius.em@gmail.com>
2020-05-18 13:37:00 +0700

    Revert "Setup redux state store"
    This reverts commit c0be036627c01e409ec408f2b359972c2caa9f7d.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import Hello from './Hello'
-import configureStore from '../configureStore'
-
-const store = configureStore()
 
 class App extends React.Component {
   render () {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' render={() => ('Home!')}/>
-            <Route path='/hello' render={() => <Hello />}/>
-          </Switch>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' render={() => ('Home!')}/>
+          <Route path='/hello' render={() => <Hello />}/>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
